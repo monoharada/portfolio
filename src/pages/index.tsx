1e17cf9b2cb09f2f0428269056e6dc202b123c94
monoharada <harada@monosus.co.jp>
2023-06-24 12:38:53 +0900

    マークアップのルールを追加しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ const home = () => {
       </Head>
       <h1>WelL come</h1>
       <p>My portolio</p>
+      <img
+        src="/hoge.png"
+        alt="image"
+        width={200}
+        height={200}
+        decoding="async"
+        loading="lazy"
+      />
     </>
   )
 }
