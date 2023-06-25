631f189ddf64686ebfcaab2d978c6560d7050ed8
monoharada <harada@monosus.co.jp>
2023-06-26 07:22:36 +0900

    スタイル、文言修正

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,7 +2,9 @@ export declare global {
   namespace JSX {
     interface IntrinsicElements {
       "stack-space": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLDivElement>,
+        React.HTMLAttributes<HTMLDivElement> & {
+          space?: "8" | "12" | "20" | "32" | "64"
+        },
         HTMLDivElement
       >
       "flex-box": React.DetailedHTMLProps<
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,22 +8,30 @@ import ThemeSwitch from "../components/themeSwitch"
 import React from "react"
 import { achievements } from "../api/achievements"
 
-
 const home = () => {
   return (
     <>
       <Head>
         <title>monoharadaのポートフォリオ</title>
-        <meta name="description" content="monoharadaのポートフォリオ、業務改善及びフロントエンドの品質・効率化をご相談ください。このページのソース:https://github.com/monoharada/portfolio" />
+        <meta
+          name="description"
+          content="monoharadaのポートフォリオ、業務改善及びフロントエンドの品質・効率化をご相談ください。このページのソース:https://github.com/monoharada/portfolio"
+        />
         <script type="module" src="/src/assets/entry.ts" />
       </Head>
       <header>
         <ThemeSwitch />
-        <a href="https://github.com/monoharada/portfolio" target="_blank" rel="noopener noreferrer">GITHUB</a>
+        <a
+          href="https://github.com/monoharada/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GITHUB
+        </a>
       </header>
       <main>
         <h1>僕は努力しない自分は嫌いだ</h1>
-        <flex-box style={{ '--align': 'end' } as React.CSSProperties} wrap>
+        <flex-box style={{ "--align": "end" } as React.CSSProperties} wrap>
           <Image
             src="/src/assets/image/img_admin.png"
             alt="my image"
@@ -35,19 +43,19 @@ const home = () => {
           />
           <DescriptionList data={descriptionList} />
         </flex-box>
-        <stack-space space='64' />
+        <stack-space space="64" />
         <section>
           <h2>スキルセットとベースライン</h2>
+          <grid-container style={{ "--grid-cols": 3 } as React.CSSProperties}>
+            <DescriptionList data={skillSets} className={"card"} />
+          </grid-container>
+          <stack-space space="32"/>
           <p>「必要な時に必要な場所へ必要なものを必要なだけ」</p>
           <p>「コーディングによって自分ではなく顧客の課題を解決する。」</p>
-          <stack-space space='32' />
-          <grid-container style={{ '--grid-cols': 3 } as React.CSSProperties}>
-          <DescriptionList data={skillSets} className={'card'} />
-          </grid-container>
         </section>
         <section>
           <h2>事例</h2>
-          <DescriptionList data={achievements} className="stack"/>
+          <DescriptionList data={achievements} className="stack" />
         </section>
       </main>
     </>
