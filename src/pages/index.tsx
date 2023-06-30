58a44f8b65fcdb8f7efd0168f61c4af5e6aaa838
monoharada <harada@monosus.co.jp>
2023-06-30 16:18:43 +0900

    ポートフォリオの更新とGoogle Tag Managerの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,31 @@ const home = () => {
           name="description"
           content="monoharadaのポートフォリオ、業務改善及びフロントエンドの品質・効率化をご相談ください。このページのソース:https://github.com/monoharada/portfolio"
         />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MKZ9B6Z');</script>
+<!-- End Google Tag Manager -->`,
+          }}
+        ></script>
+
         <script type="module" src="/src/assets/entry.ts" />
       </Head>
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MKZ9B6Z"
+          height={0}
+          width={0}
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
       <header>
         <ThemeSwitch />
         <a
@@ -49,7 +72,7 @@ const home = () => {
           <grid-container style={{ "--grid-cols": 3 } as React.CSSProperties}>
             <DescriptionList data={skillSets} className={"card"} />
           </grid-container>
-          <stack-space space="32"/>
+          <stack-space space="32" />
           <p>「必要な時に必要な場所へ必要なものを必要なだけ」</p>
           <p>「コーディングによって自分ではなく顧客の課題を解決する。」</p>
         </section>
